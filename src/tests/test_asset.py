faea6b61bb50514896c2010b471b41797f913645
MiceIik <gerson20061114@gmail.com>
2026-01-04 22:50:42 -0500

    Fixing mocking error: simulating the convertion

--- src/tests/test_asset.py
+++ src/tests/test_asset.py
@@ -3,9 +3,15 @@
 def test_load_asset(mocker):
     from src.logic.asset import AssetManager
     fake_surface = mocker.Mock()
-    load_mock = mocker.patch("pygame.image.load", return_value=fake_surface)
+    # simulate convert_alpha method
+    fake_surface.convert_alpha.return_value = fake_surface
+    load_mock = mocker.patch(
+        "src.logic.asset.pygame.image.load",
+        return_value=fake_surface
+    )
     asset_manager = AssetManager()
     a1 = asset_manager.load_image("pieces/black_pawn.png")
     a2 = asset_manager.load_image("pieces/black_pawn.png")
-    assert a1 is a2
+    assert a1 is fake_surface
+    assert a2 is fake_surface
     load_mock.assert_called_once()
