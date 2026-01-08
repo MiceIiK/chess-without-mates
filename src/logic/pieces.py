beb49cd2d3c2965b080ae0c3e823c035f1f777d9
MiceIik <gerson20061114@gmail.com>
2026-01-08 17:13:43 -0500

    Refacting Piece class and add Pawn class

--- src/logic/pieces.py
+++ src/logic/pieces.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class Piece(ABC):
+    # square is a list: [letter, num] where letter is a-h and num is 1-8
     def __init__(self, color, square):
         self.color = color
         self.square = square
@@ -11,4 +12,14 @@ class Piece(ABC):
         pass
 
     def __str__(self):
-        return f"{self.color} {self.__class__.__name__}"
+        return f"Color: {self.color} - AN: {self.__class__.__name__}"
+
+
+class Pawn(Piece):
+    def __init__(self, color, square):
+        super().__init__(color, square)
+
+    def valid_moves(self, board):
+        potential_moves = []
+        letter, num = self.square
+        
